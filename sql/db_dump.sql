269d4d69976d38d342a624ba2863733084618137
adachita <adachita@systena.co.jp>
2020-02-17 21:01:05 +0900

    SQLファイルを作成
    jobとcharacterテーブルを作成

---
+++ sql/db_dump.sql
@@ -0,0 +1,182 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.2
+-- Dumped by pg_dump version 12.2
+
+-- Started on 2020-02-17 20:58:33
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 205 (class 1259 OID 16415)
+-- Name: characters; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.characters (
+    character_id integer NOT NULL,
+    character_name text NOT NULL,
+    character_job_id integer NOT NULL
+);
+
+
+ALTER TABLE public.characters OWNER TO postgres;
+
+--
+-- TOC entry 204 (class 1259 OID 16413)
+-- Name: characters_character_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.characters_character_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.characters_character_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 2836 (class 0 OID 0)
+-- Dependencies: 204
+-- Name: characters_character_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.characters_character_id_seq OWNED BY public.characters.character_id;
+
+
+--
+-- TOC entry 203 (class 1259 OID 16404)
+-- Name: jobs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.jobs (
+    job_id integer NOT NULL,
+    job_name text NOT NULL
+);
+
+
+ALTER TABLE public.jobs OWNER TO postgres;
+
+--
+-- TOC entry 202 (class 1259 OID 16402)
+-- Name: jobs_job_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.jobs_job_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.jobs_job_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 2837 (class 0 OID 0)
+-- Dependencies: 202
+-- Name: jobs_job_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.jobs_job_id_seq OWNED BY public.jobs.job_id;
+
+
+--
+-- TOC entry 2696 (class 2604 OID 16418)
+-- Name: characters character_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.characters ALTER COLUMN character_id SET DEFAULT nextval('public.characters_character_id_seq'::regclass);
+
+
+--
+-- TOC entry 2695 (class 2604 OID 16407)
+-- Name: jobs job_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.jobs ALTER COLUMN job_id SET DEFAULT nextval('public.jobs_job_id_seq'::regclass);
+
+
+--
+-- TOC entry 2830 (class 0 OID 16415)
+-- Dependencies: 205
+-- Data for Name: characters; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.characters (character_id, character_name, character_job_id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 2828 (class 0 OID 16404)
+-- Dependencies: 203
+-- Data for Name: jobs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.jobs (job_id, job_name) FROM stdin;
+1	Warrior
+2	Wizard
+3	Monk
+\.
+
+
+--
+-- TOC entry 2838 (class 0 OID 0)
+-- Dependencies: 204
+-- Name: characters_character_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.characters_character_id_seq', 1, false);
+
+
+--
+-- TOC entry 2839 (class 0 OID 0)
+-- Dependencies: 202
+-- Name: jobs_job_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.jobs_job_id_seq', 3, true);
+
+
+--
+-- TOC entry 2700 (class 2606 OID 16423)
+-- Name: characters characters_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.characters
+    ADD CONSTRAINT characters_pkey PRIMARY KEY (character_id);
+
+
+--
+-- TOC entry 2698 (class 2606 OID 16412)
+-- Name: jobs jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.jobs
+    ADD CONSTRAINT jobs_pkey PRIMARY KEY (job_id);
+
+
+-- Completed on 2020-02-17 20:58:34
+
+--
+-- PostgreSQL database dump complete
+--
+
