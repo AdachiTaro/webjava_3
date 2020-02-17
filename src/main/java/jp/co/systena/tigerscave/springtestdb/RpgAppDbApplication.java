3d53667bd1d6ec123482b8ddfcc2e332edaf8144
adachita <adachita@systena.co.jp>
2020-02-17 20:03:41 +0900

    プロジェクト作成初期コミット

---
+++ src/main/java/jp/co/systena/tigerscave/springtestdb/RpgAppDbApplication.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.springtestdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgAppDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgAppDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/systena/tigerscave/springtestdb/RpgAppDbApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.springtestdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgAppDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
